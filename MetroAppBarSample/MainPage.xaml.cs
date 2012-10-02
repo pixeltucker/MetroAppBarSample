85cc393cecb0023a09d9752080448f51ad48ac4d
Tolga Balci <tolga.balci@gmail.com>
2012-10-01 21:06:44 -0300

    Update MetroAppBarSample/MainPage.xaml.cs
    Added DataTransfer namespace

--- MetroAppBarSample/MainPage.xaml.cs
+++ MetroAppBarSample/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.DataTransfer;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
